a9315978b4da1c5f1dcda5c5f5f4551cf620a18d
s-takuya <takuyashiratake0622@icloud.com>
2015-06-04 08:59:34 +0900

    Third commit

--- app/models/coordinate.rb
+++
@@ -1,4 +0,0 @@
-class Coordinate < ActiveRecord::Base
-  validates :person_name, presence: true
-  validates :season, presence: true
-end
--- spec/models/coordinate_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Coordinate do
-  it { is_expected.to respond_to(:person_name) }
-  it { is_expected.to respond_to(:season) }
-  it { is_expected.to respond_to(:occupation) }
-end
