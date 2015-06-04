a7f85b52fd24a9400e734c20812999aad9ae591a
s-takuya <takuyashiratake0622@icloud.com>
2015-06-04 09:28:13 +0900

    Write tests for coordinate model

---
+++ spec/models/coordinate_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Coordinate do
+  subject(:coordinate) { FactoryGirl.create(:coordinate) }
+
+  it { is_expected.to respond_to(:person_name) }
+  it { is_expected.to respond_to(:occupation) }
+  it { is_expected.to respond_to(:dress_season) }
+
+  describe 'person name' do
+    context 'when it is nil' do
+      before { coordinate.person_name = nil }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:person_name].size).to eq(1)
+      end
+    end
+
+    context 'when it is empty' do
+      before { coordinate.person_name = '' }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:person_name].size).to eq(1)
+      end
+    end
+  end
+
+  describe 'occupation' do
+    context 'when it is nil' do
+      before { coordinate.occupation = nil }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:occupation].size).to eq(1)
+      end
+    end
+
+    context 'when it is empty' do
+      before { coordinate.occupation = '' }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:occupation].size).to eq(1)
+      end
+    end
+  end
+
+  describe 'dress season' do
+    context 'when it is nil' do
+      before { coordinate.dress_season = nil }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:dress_season].size).to eq(1)
+      end
+    end
+
+    context 'when it is empty' do
+      before { coordinate.dress_season = '' }
+
+      it 'has an error on person name' do
+        expect(coordinate).not_to be_valid
+        expect(coordinate.errors[:dress_season].size).to eq(1)
+      end
+    end
+  end
+end
