5e540463b6292641c72ea7449cfcfde943c63dd1
s-takuya <takuyashiratake0622@icloud.com>
2015-06-04 09:29:03 +0900

    Change property name season to dress_season

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,5 +1,5 @@
 class Coordinate < ActiveRecord::Base
-  validates :person_name, persence: true
-  validates :season, presence: true
+  validates :person_name, presence: true
+  validates :dress_season, presence: true
   validates :occupation, presence: true
 end
