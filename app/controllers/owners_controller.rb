d26eaa8c4ed1b532ba40ef00f4532625b04d8b87
s-takuya <takuyashiratake0622@icloud.com>
2016-02-21 18:57:15 +0900

    shuffle method

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,10 +1,10 @@
 class OwnersController < ApplicationController
   def index
-    @owners = Owner.recent
+    @owners = Owner.all.shuffle
   end
 
   def show
     @owner = Owner.find(params[:id])
-    @coordinates = @owner.coordinates.includes(:photos)
+    @coordinates = @owner.coordinates.includes(:photos).shuffle
   end
 end
