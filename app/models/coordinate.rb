872beb449ac4767a9bf7501f5b0e0fc937dd1cc1
s-takuya <takuyashiratake0622@icloud.com>
2016-02-29 00:56:25 +0900

    previouts

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -11,4 +11,12 @@ class Coordinate < ActiveRecord::Base
   def self.search(owner_id)
     where(owner_id: owner_id.to_i)
   end
+
+  def previous(owner, id)
+    owner.coordinates.where('id < ?', id).order('id DESC').first
+  end
+
+  def next(owner, id)
+    owner.coordinates.where('id > ?', self.id).order('id ASC').first
+  end
 end
