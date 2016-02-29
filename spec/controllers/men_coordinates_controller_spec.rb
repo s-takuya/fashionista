16731cc34fd97b23656496752113129ecc0f0c76
s-takuya <takuyashiratake0622@icloud.com>
2016-02-29 21:14:51 +0900

    Add test code

--- spec/controllers/men_coordinates_controller_spec.rb
+++ spec/controllers/men_coordinates_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe MenCoordinatesController, type: :controller do
+RSpec.describe MenCoordinatesController do
+  let(:coordinate) { FactoryGirl.create(:coordinate, owner: owner) }
+  let(:owner) { FactoryGirl.create(:owner) }
 
+  describe 'GET index' do
+    it 'assigns instance variable' do
+      get :index, owner_id: owner.id
+      expect(assigns(:coordinates)).to contain_exactly(coordinate)
+    end
+  end
+
+  describe 'GET show' do
+    it 'assigns instance variable' do
+      get :show, id: coordinate.id
+      expect(assigns(:coordinate)).to eq coordinate
+    end
+  end
 end
--- spec/factories/owners.rb
+++ spec/factories/owners.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :owner do
     name 'beckham'
     occupation 'former soccer player'
+    man_flag true
   end
 end
